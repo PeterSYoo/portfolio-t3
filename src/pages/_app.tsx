c9d9ccfc225b1283176a9695e5717525a4c0f05a
Peter Yoo <trustintofu@gmail.com>
2023-03-25 02:06:37 -0700

    feat(MyApp): Adds favicon.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,7 +6,6 @@ import { api } from "~/utils/api";
 
 import "~/styles/globals.css";
 import Head from "next/head";
-import { Header } from "~/components/root/Header.components";
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
@@ -17,7 +16,26 @@ const MyApp: AppType<{ session: Session | null }> = ({
       <Head>
         <title>Peter Yoo - Portfolio</title>
         <meta name="description" content="Peter Yoo's Portfolio" />
-        <link rel="icon" href="/favicon.ico" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+        <meta name="msapplication-TileColor" content="#da532c" />
+        <meta name="theme-color" content="#ffffff" />
       </Head>
       <div className="min-w-screen flex min-h-screen flex-col bg-[#EFEEED]">
         <div className="mt-[110px] flex flex-grow">
