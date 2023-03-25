96af8fc01cf8ea5b9e3316e25c45fa4cc975312d
Peter Yoo <trustintofu@gmail.com>
2023-03-25 02:13:54 -0700

    feat(Home): Adds state to update string related to section element id's, and passes it to Header to be used to update the activeMenu state in order to move the menu icon bg color.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,39 +1,96 @@
 import { type NextPage } from "next";
-import { BsGithub, BsLinkedin } from "react-icons/bs";
-import { AiOutlineMail } from "react-icons/ai";
-import { Header } from "~/components/root/Header.components";
+import { useEffect, useRef, useState } from "react";
+import { Header } from "~/components/Header.components";
+import { LandingSection } from "~/components/landing-section/LandingSection.components";
+import { WorkSection } from "~/components/work-section/WorkSection.components";
 
 const Home: NextPage = () => {
-  // JSX --------------------------------------
+  // States --------------------------------------
+  const [scrollActiveMenu, setScrollActiveMenu] = useState<string>("landing");
+
+  // Refs ----------------------------------
+  const sectionRefs = {
+    landing: useRef<HTMLElement>(null),
+    work: useRef<HTMLElement>(null),
+    about: useRef<HTMLElement>(null),
+    play: useRef<HTMLElement>(null),
+  };
+
+  const isButtonScrollingRef = useRef<boolean>(false);
+
+  // Custom Functions --------------------------------------
+  const handleScroll = () => {
+    if (!isButtonScrollingRef.current) {
+      for (const [sectionId, sectionRef] of Object.entries(sectionRefs)) {
+        const section = sectionRef.current;
+        if (section) {
+          const sectionRect = section.getBoundingClientRect();
+          if (sectionRect.top <= window.innerHeight / 2) {
+            setScrollActiveMenu(sectionId);
+          }
+        }
+      }
+    }
+  };
+
+  // Effects ------------------------------------
+  useEffect(() => {
+    const handleScroll = () => {
+      for (const [sectionId, sectionRef] of Object.entries(sectionRefs)) {
+        const section = sectionRef.current;
+        if (section) {
+          const sectionRect = section.getBoundingClientRect();
+          if (sectionRect.top <= window.innerHeight / 2) {
+            setScrollActiveMenu(sectionId);
+          }
+        }
+      }
+    };
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  useEffect(() => {
+    isButtonScrollingRef.current = false;
+  }, [scrollActiveMenu]);
+
+  console.log({ scrollActiveMenu });
+
+  // JSX ---------------------------------------------------
   return (
     <>
-      <Header />
-      <main className="flex w-full">
-        <div className="-mt-[110px] flex h-screen w-screen justify-center">
-          <div className="flex w-full max-w-[1005px] flex-col gap-[30px] px-5 md:gap-[40px]">
-            {/* Name ---------------- */}
-            <div className="mt-[200px]">
-              <h1 className="text-[64px] font-bold leading-[80px] tracking-tight">
-                Peter Yoo
-              </h1>
-              <div className="flex items-center gap-5">
-                <p className="pl-1 text-[20px]">
-                  Full Stack
-                  Developer&nbsp;&nbsp;&nbsp;·&nbsp;&nbsp;&nbsp;Artist&nbsp;&nbsp;&nbsp;·
-                </p>
-                <BsLinkedin className="text-xl text-[#0077B7]" />
-                <BsGithub className="text-xl" />
-                <AiOutlineMail className="text-2xl text-[#ff3e5e]" />
-              </div>
-            </div>
-            {/* Animated Text ---------------- */}
-            <div className="">
-              <h1 className="font-serif text-[76px]">
-                Always curious about learning new technologies.
-              </h1>
-            </div>
-          </div>
-        </div>
+      <Header
+        scrollActiveMenu={scrollActiveMenu}
+        isButtonScrollingRef={isButtonScrollingRef}
+      />
+      <main className="flex w-full flex-col">
+        {/* Landing Section --------------------------- */}
+        <LandingSection landingRef={sectionRefs.landing} />
+        {/* Work Section ------------------------------ */}
+        <WorkSection workRef={sectionRefs.work} />
+        {/* About Section ----------------------------- */}
+        <section
+          ref={sectionRefs.about}
+          id="about"
+          className="h-screen bg-red-200"
+        ></section>
+        {/* Play Section ------------------------------ */}
+        <section
+          ref={sectionRefs.play}
+          id="play"
+          className="h-screen bg-yellow-200"
+        ></section>
+        {/* Footer Section ---------------------------- */}
+        <section id="footer" className="h-[400px] bg-purple-300"></section>
       </main>
     </>
   );
