1fe622d8604d174fc98011d7b6d22071bbb20c2d
Peter Yoo <trustintofu@gmail.com>
2023-03-26 02:33:24 -0700

    feat(additional.d.ts): Adds interface for work object.

---
+++ additional.d.ts
@@ -0,0 +1,13 @@
+export interface IWork {
+  name: string;
+  type: string;
+  date: string;
+  thumbnail: string;
+  technologiesThumb: string;
+  link: string;
+  github: string;
+  technologies: string[];
+  description: string;
+  images: string[];
+  youtube: string;
+}
