d320f5de8f39e1881b0f4c2dbdf9a75e0ff64872
Peter Yoo <trustintofu@gmail.com>
2023-03-26 02:35:04 -0700

    feat(MyApp): Wrapps app with IsWorkProvider.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,6 +6,7 @@ import { api } from "~/utils/api";
 
 import "~/styles/globals.css";
 import Head from "next/head";
+import { IsWorkProvider } from "~/contexts/IsWorkContext";
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
@@ -13,35 +14,37 @@ const MyApp: AppType<{ session: Session | null }> = ({
 }) => {
   return (
     <SessionProvider session={session}>
-      <Head>
-        <title>Peter Yoo - Portfolio</title>
-        <meta name="description" content="Peter Yoo's Portfolio" />
-        <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href="/apple-touch-icon.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-          href="/favicon-32x32.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
-          href="/favicon-16x16.png"
-        />
-        <link rel="manifest" href="/site.webmanifest" />
-        <meta name="msapplication-TileColor" content="#da532c" />
-        <meta name="theme-color" content="#ffffff" />
-      </Head>
-      <div className="min-w-screen flex min-h-screen flex-col bg-[#EFEEED]">
-        <div className="mt-[110px] flex flex-grow">
-          <Component {...pageProps} />
+      <IsWorkProvider>
+        <Head>
+          <title>Peter Yoo - Portfolio</title>
+          <meta name="description" content="Peter Yoo's Portfolio" />
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/apple-touch-icon.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/favicon-16x16.png"
+          />
+          <link rel="manifest" href="/site.webmanifest" />
+          <meta name="msapplication-TileColor" content="#da532c" />
+          <meta name="theme-color" content="#ffffff" />
+        </Head>
+        <div className="min-w-screen flex min-h-screen flex-col bg-[#EFEEED]">
+          <div className="mt-[110px] flex flex-grow">
+            <Component {...pageProps} />
+          </div>
         </div>
-      </div>
+      </IsWorkProvider>
     </SessionProvider>
   );
 };
