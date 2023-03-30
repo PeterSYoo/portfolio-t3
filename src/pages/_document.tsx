33c3dd330ec28b1f6b15bc066aa5581434907422
Peter Yoo <trustintofu@gmail.com>
2023-03-30 00:40:51 -0700

    feat: Moves google analytics to document.

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,9 +1,25 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
+import { env } from "~/env.mjs";
 
 export default function Document() {
   return (
     <Html style={{ scrollBehavior: "smooth" }}>
       <Head />
+      {/* Google Tag */}
+      <Script
+        src={`https://www.googletagmanager.com/gtag/js?id=${env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', '${env.NEXT_PUBLIC_GOOGLE_ANALYTICS}');
+          `}
+      </Script>
       <body>
         <Main />
         <NextScript />
